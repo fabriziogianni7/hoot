a4d65eb8cdf517eb624c2b5b29385a4fb4a727c9
lzmrd <vicari.fed@gmail.com>
2025-10-24 11:29:25 +0200

    fix: restore correct timer margin (mt-6 instead of mb-6)

--- frontend/app/quiz/play/page.tsx
+++ frontend/app/quiz/play/page.tsx
@@ -314,7 +314,7 @@ function PlayQuizContent() {
       </div>
 
               {/* Timer circle */}
-              <div className="mb-6 flex justify-center">
+              <div className="mt-6 flex justify-center">
           <div className="relative">
             <svg className="w-20 h-20 transform -rotate-90" viewBox="0 0 36 36">
               {/* Background circle */}
