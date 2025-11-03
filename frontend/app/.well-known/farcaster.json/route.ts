cfe7af6774f9774ddccc4765604ee8b9825cccf1
Fabriziogianni7 <fabrizio@onbeam.com>
2025-11-03 14:41:15 +0100

    test change to minikit

--- frontend/app/.well-known/farcaster.json/route.ts
+++ frontend/app/.well-known/farcaster.json/route.ts
@@ -2,5 +2,6 @@ import { withValidManifest } from "@coinbase/onchainkit/minikit";
 import { minikitConfig } from "../../../minikit.config";
 
 export async function GET() {
-  return Response.json(withValidManifest(minikitConfig));
+  // return Response.json(withValidManifest(minikitConfig));
+  return Response.json(minikitConfig);
 }
